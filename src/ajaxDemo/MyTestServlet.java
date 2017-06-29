eb88963babf13e9dc54c02f61717182194fb89ed
libie <libie_lt@126.com>
2017-06-29 17:23:22 +0800

    myServlet
    create Servlet

---
+++ src/ajaxDemo/MyTestServlet.java
@@ -0,0 +1,23 @@
+package ajaxDemo;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyTestServlet extends HttpServlet {
+
+	
+public void doGet(HttpServletRequest request,HttpServletResponse response) 
+		throws ServletException{
+	
+}
+
+public void doPost(HttpServletRequest request,HttpServletResponse response) 
+		throws ServletException{
+	
+}
+
+
+
+}
